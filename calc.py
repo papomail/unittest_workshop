eb77b35f93d12c838e83afbeda995ea036292bf9
papo <papomail@gmail.com>
2020-05-21 21:14:52 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,10 +5,19 @@ def substract(a,b):
     return a - b 
 
 def multiply(a,b):
-    return a * b
+    return a ** b
 
 def divide(a,b):
     if b == 0:
         raise ValueError('Cannot divide by 0')
     return a/b
 
+
+
+
+
+
+def complicated_function(a,b,c,d,e,f):
+    the_value=multiply(substract(add(a,b),divide(d,c)),e)
+    print(f'The value you need is {the_value}')
+    
